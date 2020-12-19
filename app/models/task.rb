2ce8383c3a8502f71f549f860e25ffb6009f26c7
tomomi ogino <s4tm83@gmail.com>
2020-12-19 18:24:49 +0900

    add: バリデーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,11 @@ class Task < ApplicationRecord
   with_options presence: true do
     validates :title, length: { maximum: 100 }
     validates :content, length: { maximum: 300 }
+    validates :deadline
+  end
+  validate :date_not_before_now
+
+  def date_not_before_now
+    errors.add(:deadline, I18n.t('errors.messages.invalid_date')) if deadline.nil? || deadline < DateTime.current
   end
 end
