497599d6d1b8cc33e444fbd7be952c96dcc75545
tomomi ogino <s4tm83@gmail.com>
2020-12-19 18:36:48 +0900

    add: タスクに終了期限を登録できるよう実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :deadline)
   end
 
   def set_task
