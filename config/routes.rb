4945dcb0f8547765a8313c414730f224cbf43cd4
tomomi ogino <s4tm83@gmail.com>
2020-12-24 16:55:24 +0900

    add: 管理画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  namespace :admin do
+    resources :users
+  end
 end
