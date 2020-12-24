d9ed27f6f7e0848c1a25a39bf8c2ff86d659f084
tomomi ogino <s4tm83@gmail.com>
2020-12-24 14:32:43 +0900

    add: usersテーブル作成とバリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: { maximum: 50 }
+    validates :email, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+    validates :password, length: {minimum: 6}, on: :create
+  end
+  before_validation {email.downcase!}
+  has_secure_password
+end
