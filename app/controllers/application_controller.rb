99329a2382d1e2d1d8bb92a306ebf87aa2fdd5ab
tomomi ogino <s4tm83@gmail.com>
2020-12-26 23:35:47 +0900

    update: if文のネスト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,8 @@ class ApplicationController < ActionController::Base
   end
 
   def correct_user_with_task
-    if current_user.admin == false
-      if current_user.id != Task.find(params[:id].to_i).user_id
-        redirect_to tasks_path, flash: {danger: t('alert.users.Another user')}
-      end
+    if current_user.admin == false && current_user.id != Task.find(params[:id].to_i).user_id
+      redirect_to tasks_path, flash: {danger: t('alert.users.Another user')}
     end
   end
 end
